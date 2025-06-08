55639b2be4635fb1904e9755952b2d9d13a982b7
fernandobn <154335390+fernandobn@users.noreply.github.com>
2025-06-08 14:14:16 -0500

    Campos
    Formulario de estudiante cambiado y grafico igual

--- Aplicaciones/Prediccion/urls.py
+++ Aplicaciones/Prediccion/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('formularioEstudiantil/', views.formularioEstudiantil, name='formularioEstudiantil'),
-    path('formularioSalud/', views.formularioSalud, name='formularioSalud'),
-    path('graficoSalud/', views.graficoSalud, name='graficoSalud'),
+    #path('formularioSalud/', views.formularioSalud, name='formularioSalud'),
+    #path('graficoSalud/', views.graficoSalud, name='graficoSalud'),
     path('graficoEstudiantil/', views.graficoEstudiantil, name='graficoEstudiantil'),
     path('integrantes/', views.integrantes, name='integrantes'),
 
--- Aplicaciones/Prediccion/views.py
+++ Aplicaciones/Prediccion/views.py
@@ -15,70 +15,14 @@ def inicio(request):
 
 # Prediccion estudiantil
 def formularioEstudiantil(request):
-    if request.method == 'POST':
-        horas_estudio_semanal = int(request.POST.get('horas_estudio_semanal'))
-        asistencia = int(request.POST.get('asistencia'))
-        participacion = int(request.POST.get('participacion'))
-        evaluacion_parcial1 = float(request.POST.get('evaluacion_parcial1'))
-        evaluacion_parcial2 = float(request.POST.get('evaluacion_parcial1'))
-        evaluacion_parcial3 = float(request.POST.get('evaluacion_parcial1'))
-
-        modelo_lineal = joblib.load('Aplicaciones/Prediccion/modelo_lineal_notas.pkl')
-        modelo_logistico = joblib.load('Aplicaciones/Prediccion/modelo_logistico_notas.pkl')
-        
-        prediccion = modelo_lineal.predict([[horas_estudio_semanal, asistencia, participacion, evaluacion_parcial1, evaluacion_parcial2, evaluacion_parcial3]])
-        prediccion_logistica = modelo_logistico.predict([[horas_estudio_semanal, asistencia, participacion, evaluacion_parcial1, evaluacion_parcial2, evaluacion_parcial3, prediccion[0]]])
-
-        # --- Generar gráficos ---
-        # Gráfico 1: Predicción lineal (Nota final)
-        plt.figure(figsize=(8, 5))
-        plt.bar(['Nota Final'], [prediccion[0]], color='skyblue', edgecolor='black')
-        plt.ylim(0, 37)  # Nota entre 0 y 10
-        plt.ylabel('Nota Predicha')
-        plt.title(f'Predicción de Nota Final: {prediccion[0]:.2f}')
-        plt.grid(axis='y', linestyle='--', alpha=0.7)
-
-        # Guardar en buffer
-        buf_lineal = BytesIO()
-        plt.savefig(buf_lineal, format='png')
-        plt.close()
-        buf_lineal.seek(0)
-        grafico_lineal = base64.b64encode(buf_lineal.read()).decode('utf-8')
-
-        # Gráfico 2: Modelo Logistico
-        plt.figure(figsize=(8, 2))
-        plt.barh(['Riesgo'], [prediccion_logistica[0]], color='skyblue', edgecolor='black')
-        plt.xlim(0, 1)
-        plt.xlabel('Probabilidad')
-        plt.title(f'Riesgo de Fracaso: {prediccion_logistica[0]:.2%}')
-        plt.grid(axis='x', linestyle='--', alpha=0.7)
-        
-        buf_log = BytesIO()
-        plt.savefig(buf_log, format='png')
-        plt.close()
-        buf_log.seek(0)
-        grafico_logistico = base64.b64encode(buf_log.read()).decode('utf-8')
-
-
-        # Contexto para el template
-        contexto = {
-            'grafico_lineal': grafico_lineal,
-            'grafico_logistico': grafico_logistico,
-            'nota_final': f'{prediccion[0]:.2f}',
-            'riesgo_fracaso': prediccion_logistica[0]
-        }
-
-        return render(request, 'graficoEstudiantil.html', contexto)
-
-
-
-    return render(request, 'formularioEstudiantil.html')
+    horas = range(0, 24)  # 0 a 23 inclusive
+    return render(request, 'formularioEstudiantil.html', {'horas': horas})
 
 
 
 
 # Prediccion salud
-def formularioSalud(request):
+#def formularioSalud(request):
     if request.method == 'POST':
         edad = int(request.POST.get('edad'))
         imc = float(request.POST.get('imc'))
@@ -138,7 +82,7 @@ def formularioSalud(request):
 
 
 # graficos de prediccion
-def graficoSalud(request):
+#def graficoSalud(request):
     return render(request, 'graficoSalud.html')
 
 def graficoEstudiantil(request):
